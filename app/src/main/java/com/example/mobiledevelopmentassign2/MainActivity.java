e4ac5a43749a5b23b64881e6f18e8096711fab6f
Lucy Trainor <adamt2005@icloud.com>
2025-03-31 14:36:57 +0100

    Started doing buttons for navigation

--- app/src/main/java/com/example/mobiledevelopmentassign2/MainActivity.java
+++ app/src/main/java/com/example/mobiledevelopmentassign2/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.mobiledevelopmentassign2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +12,30 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button home_btn;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        home_btn = (Button) findViewById(R.id.home_button);
+
+        home_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+
+
 }
